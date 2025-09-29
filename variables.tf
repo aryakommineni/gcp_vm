c5872cfe69768228565a2ad37620e39cae0e77a9
aryakommineni <147579531+aryakommineni@users.noreply.github.com>
2025-09-29 16:59:12 -0700

    variables.tf

---
+++ variables.tf
@@ -0,0 +1,6 @@
+variable "project_id"     { type = string }
+variable "region"         { type = string }
+variable "zone"           { type = string }
+variable "machine_type"   { type = string }
+variable "vm_name"        { type = string }
+variable "credentials_file" { type = string }
